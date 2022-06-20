2d09b808001f0223dfe1d6d927aa1a772a631d24
lustomas <102303504+lustomas@users.noreply.github.com>
2022-06-20 15:18:49 +0200

    Update tmdb_client.py

--- tmdb_client.py
+++ tmdb_client.py
@@ -3,11 +3,12 @@ import random
 
 API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYTM0ZWJmMWE4MzgwZGIyZTFjMjc5YzQyYTNiNGM2YSIsInN1YiI6IjYyOWIxNWIxNTUwN2U5MTJjNzkzMGY2ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VRBKTvOYSwiA_Jplf5Md7RHI00LzCUvjR0bsUhn4DEM"
 
+def header():
+    "Authorization": f"Bearer {API_TOKEN}" 
+    
 def get_popular_movies():
     endpoint = "https://api.themoviedb.org/3/movie/popular"
-    headers = {
-        "Authorization": f"Bearer {API_TOKEN}"
-    }
+    headers = header()
     response = requests.get(endpoint, headers=headers)
     return response.json()
 
@@ -17,9 +18,7 @@ def get_poster_url(poster_api_path, size="w342"):
 
 def get_movies(list_type, how_many):
     endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
-    headers = {
-        "Authorization": f"Bearer {API_TOKEN}"
-    }
+    headers = header()
     response = requests.get(endpoint, headers=headers)
     response.raise_for_status()
     data = response.json()
@@ -28,34 +27,26 @@ def get_movies(list_type, how_many):
 
 def get_movies_list(list_type):
     endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
-    headers = {
-        "Authorization": f"Bearer {API_TOKEN}"
-    }
+    headers = header()
     response = requests.get(endpoint, headers=headers)
     response.raise_for_status()
     return response.json()
 
 def get_single_movie(movie_id):
     endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
-    headers = {
-        "Authorization": f"Bearer {API_TOKEN}"
-    }
+    headers = header()
     response = requests.get(endpoint, headers=headers)
     return response.json()
 
 def get_single_movie_cast(movie_id):
     endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
-    headers = {
-        "Authorization": f"Bearer {API_TOKEN}"
-    }
+    headers = header()
     response = requests.get(endpoint, headers=headers)
     return response.json()["cast"]
 
 def get_movie_images(movie_id):
     endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/images"
-    headers = {
-        "Authorization": f"Bearer {API_TOKEN}"
-    }
+    headers = header()
     response = requests.get(endpoint, headers=headers)
     return response.json()
 
